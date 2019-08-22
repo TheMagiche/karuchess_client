786e1d4ad1167b39128aebfb50c7d6afc9cb7d71
themagiche <magiche.mc@gmail.com>
2019-08-22 14:35:27 +0300

    fixed site login redirect

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -47,7 +47,7 @@ export default {
   methods: {
     logout: function() {
       this.$store.dispatch("logout").then(() => {
-        this.$router.push("/login");
+        this.$router.push("/site-login");
       });
     }
   },
